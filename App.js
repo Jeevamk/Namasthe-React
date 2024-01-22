60f5260f37043ba9fc6692876561e3a7108984b7
Jeevamk <138573828+Jeevamk@users.noreply.github.com>
2024-01-22 17:09:46 +0530

    setup completed

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
-const parent = React.createElement("div",{id:'parent'},[React.createElement('div',{id:'child'},[React.createElement('h1',{},"h1 tag"),React.createElement('h2',{},"h2 tag")])],React.createElement('div',{id:'child'},[React.createElement('h1',{},"h1 tag"),React.createElement('h2',{},"h2 tag")]))
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent)
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  [
+    React.createElement("div", { id: "child" }, [
+      React.createElement("h1", {}, "Namaste React 💖"),
+      React.createElement("h2", {}, "by jeeva"),
+    ]),
+  ],
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "h1 tag"),
+    React.createElement("h2", {}, "h2 tag"),
+  ])
+);
 
-
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
 
 // const heading = React.createElement("h1",{id:"head", style :{color:"brown"}},"hello world from react")
 // const root = ReactDOM.createRoot(document.getElementById('root'))
 // root.render(heading)
-
-
